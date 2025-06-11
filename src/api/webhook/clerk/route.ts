5719f44eaf49dae2cfb9b7cfd50bb0c6bb81bc5e
DortaEdward <dortaedward@gmail.com>
2025-06-11 15:08:15 -0400

    added clerk webhook api route

---
+++ src/api/webhook/clerk/route.ts
@@ -0,0 +1,87 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { clerkClient, type WebhookEvent } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import UserQueries from '~/server/db/user.queries'
+
+export async function POST(req: Request) {
+  const CLERK_WEBHOOK_SIGNING_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET
+  if (!CLERK_WEBHOOK_SIGNING_SECRET) {
+    throw new Error('Error: Please add CLERK_WEBHOOK_SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+  }
+
+  const wh = new Webhook(CLERK_WEBHOOK_SIGNING_SECRET)
+
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error: Missing Svix headers', {
+      status: 400,
+    })
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  let evt: WebhookEvent
+
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error: Could not verify webhook:', err)
+    return new Response('Error: Verification error', {
+      status: 400,
+    })
+  }
+
+  if (evt.type === "user.created") {
+    const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
+    try {
+      const newUserId = await UserQueries.CreateUser(id,email_addresses[0]!.email_address,first_name!,last_name!)
+      if (newUserId) {
+        const client = await clerkClient()
+        await client.users.updateUserMetadata(id, {
+          publicMetadata: {
+            userId: newUserId
+          }
+        })
+      }
+      return NextResponse.json({ message: "OK" })
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  if (evt.type === "user.updated") {
+    // updated user
+  }
+
+  // Delete User in Db
+  if (evt.type === "user.deleted") {
+    try {
+      // const deletedUser = await db.user.delete({
+      //   where: {
+      //     clerkId: evt.data.id,
+      //   }
+      // })
+      // console.log("Deleted User: ", deletedUser.id)
+
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  return new Response('Webhook received', { status: 200 })
+}
